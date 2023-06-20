796f08af1c2d7619c3b9b117e220c9043164db57
huggy <mmaruna2013@gmail.com>
2023-06-21 00:56:47 +0200

    Updated test to use new standard

--- src/test/java/com/example/zavrsnirad/controller/UserControllerTest.java
+++ src/test/java/com/example/zavrsnirad/controller/UserControllerTest.java
@@ -9,21 +9,26 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.core.oidc.StandardClaimNames;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.util.List;
+
 import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(controllers = UserController.class)
 @RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 class UserControllerTest {
     @Autowired
     private MockMvc mockMvc;
@@ -35,7 +40,6 @@ class UserControllerTest {
     private UserProfileService userProfileService;
 
     @Test
-    @WithMockUser(username = "testUser", password = "iloveyou", roles = "ADMIN")
     @DisplayName("Test update password success")
     public void updatePasswordTest() throws Exception {
         //given
@@ -43,19 +47,19 @@ class UserControllerTest {
         ObjectMapper objectMapper = new ObjectMapper();
 
         //when
-        when(userService.updatePassword("token", updatePasswordDTO)).thenReturn(ResponseEntity.ok("OK"));
+        when(userService.updatePassword(null, updatePasswordDTO)).thenReturn(ResponseEntity.ok("OK"));
 
         //then
         mockMvc.perform(MockMvcRequestBuilders.post("/api/user/password/update")
-                        .with(SecurityMockMvcRequestPostProcessors.csrf())
-                        .header("Authorization", "Bearer token")
+                        .with(jwt().authorities(List.of(new SimpleGrantedAuthority("SCOPE_ADMIN")))
+                                .jwt(jwt -> jwt.claim(StandardClaimNames.PREFERRED_USERNAME, "admin")))
+                        .header("Authorization", "")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(updatePasswordDTO)))
                 .andExpect(status().isOk());
     }
 
     @Test
-    @WithMockUser(username = "testUser", password = "iloveyou", roles = "ADMIN")
     @DisplayName("Test update password fail no Authorization header")
     public void updatePasswordNoAuthorizationToken() throws Exception {
         UpdatePasswordDTO updatePasswordDTO = new UpdatePasswordDTO("iloveyou", "iloveyou2", "iloveyou2");
@@ -66,7 +70,8 @@ class UserControllerTest {
 
         //then
         mockMvc.perform(MockMvcRequestBuilders.post("/api/user/password/update")
-                        .with(SecurityMockMvcRequestPostProcessors.csrf())
+                        .with(jwt().authorities(List.of(new SimpleGrantedAuthority("SCOPE_ADMIN")))
+                                .jwt(jwt -> jwt.claim(StandardClaimNames.PREFERRED_USERNAME, "admin")))
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(updatePasswordDTO)))
                 .andExpect(status().isBadRequest())
@@ -74,39 +79,38 @@ class UserControllerTest {
     }
 
     @Test
-    @WithMockUser
     @DisplayName("Test update password fail no Update Password DTO")
-    public void updatepasswordNoUpdatePasswordDTO() throws Exception {
+    public void updatePasswordNoUpdatePasswordDTO() throws Exception {
         //given
         UpdatePasswordDTO updatePasswordDTO = new UpdatePasswordDTO("iloveyou", "iloveyou2", "iloveyou2");
         ObjectMapper objectMapper = new ObjectMapper();
 
         //when
-        when(userService.updatePassword("token", updatePasswordDTO)).thenReturn(ResponseEntity.ok("OK"));
+        when(userService.updatePassword(null, updatePasswordDTO)).thenReturn(ResponseEntity.ok("OK"));
 
         //then
         mockMvc.perform(MockMvcRequestBuilders.post("/api/user/password/update")
-                        .with(SecurityMockMvcRequestPostProcessors.csrf())
-                        .header("Authorization", "Bearer token"))
+                        .with(jwt().authorities(List.of(new SimpleGrantedAuthority("SCOPE_ADMIN")))
+                                .jwt(jwt -> jwt.claim(StandardClaimNames.PREFERRED_USERNAME, "admin")))
+                        .header("Authorization", ""))
                 .andExpect(status().isBadRequest());
     }
 
     @Test
-    @WithMockUser(username = "testUser", password = "iloveyou", roles = "ADMIN")
     @DisplayName("Test get self success")
     public void testGetSelf() throws Exception {
         //when
-        when(userProfileService.getSelf("token")).thenReturn(ResponseEntity.ok().build());
+        when(userProfileService.getSelf(null)).thenReturn(ResponseEntity.ok().build());
 
         //then
         mockMvc.perform(MockMvcRequestBuilders.get("/api/user/profile")
-                .with(SecurityMockMvcRequestPostProcessors.csrf())
-                .header("Authorization", "Bearer token"))
+                        .with(jwt().authorities(List.of(new SimpleGrantedAuthority("SCOPE_ADMIN")))
+                                .jwt(jwt -> jwt.claim(StandardClaimNames.PREFERRED_USERNAME, "admin")))
+                .header("Authorization", ""))
                 .andExpect(status().isOk());
     }
 
     @Test
-    @WithMockUser(username = "testUser", password = "iloveyou", roles = "ADMIN")
     @DisplayName("Test get self fail no Authorization header")
     public void testGetSelfAndFailNoToken() throws Exception {
         //when
@@ -114,14 +118,14 @@ class UserControllerTest {
 
         //then
         mockMvc.perform(MockMvcRequestBuilders.get("/api/user/profile")
-                        .with(SecurityMockMvcRequestPostProcessors.csrf()))
+                        .with(jwt().authorities(List.of(new SimpleGrantedAuthority("SCOPE_ADMIN")))
+                            .jwt(jwt -> jwt.claim(StandardClaimNames.PREFERRED_USERNAME, "admin"))))
                 .andExpect(status().isBadRequest())
                 .andExpect(status().reason("Required header 'Authorization' is not present."));
 
     }
 
     @Test
-    @WithMockUser(username = "testUser", password = "iloveyou", roles = "ADMIN")
     @DisplayName("Test updateProfile success")
     public void testUpdateProfile() throws Exception {
         //given
@@ -129,19 +133,19 @@ class UserControllerTest {
         ObjectMapper objectMapper = new ObjectMapper();
 
         //when
-        when(userProfileService.updateSelfProfile("token", data)).thenReturn(ResponseEntity.ok().build());
+        when(userProfileService.updateSelfProfile(null, data)).thenReturn(ResponseEntity.ok().build());
 
         //then
         mockMvc.perform(MockMvcRequestBuilders.put("/api/user/profile/update")
-                .with(SecurityMockMvcRequestPostProcessors.csrf())
-                .header("Authorization", "Bearer token")
+                        .with(jwt().authorities(List.of(new SimpleGrantedAuthority("SCOPE_ADMIN")))
+                                .jwt(jwt -> jwt.claim(StandardClaimNames.PREFERRED_USERNAME, "admin")))
+                .header("Authorization", "")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(data)))
                 .andExpect(status().isOk());
     }
 
     @Test
-    @WithMockUser(username = "testUser", password = "iloveyou", roles = "ADMIN")
     @DisplayName("Test updateProfile fail no token")
     public void testUpdateProfileNoToken() throws Exception {
         //given
@@ -153,7 +157,8 @@ class UserControllerTest {
 
         //then
         mockMvc.perform(MockMvcRequestBuilders.put("/api/user/profile/update")
-                        .with(SecurityMockMvcRequestPostProcessors.csrf())
+                        .with(jwt().authorities(List.of(new SimpleGrantedAuthority("SCOPE_ADMIN")))
+                                .jwt(jwt -> jwt.claim(StandardClaimNames.PREFERRED_USERNAME, "admin")))
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(data)))
                 .andExpect(status().isBadRequest())
@@ -161,7 +166,6 @@ class UserControllerTest {
     }
 
     @Test
-    @WithMockUser(username = "testUser", password = "iloveyou", roles = "ADMIN")
     @DisplayName("Test updateProfile fail no data")
     public void testUpdateProfileNoData() throws Exception {
         //given
@@ -169,12 +173,13 @@ class UserControllerTest {
         ObjectMapper objectMapper = new ObjectMapper();
 
         //when
-        when(userProfileService.updateSelfProfile("token", data)).thenReturn(ResponseEntity.ok().build());
+        when(userProfileService.updateSelfProfile(null, data)).thenReturn(ResponseEntity.ok().build());
 
         //then
         mockMvc.perform(MockMvcRequestBuilders.put("/api/user/profile/update")
-                        .with(SecurityMockMvcRequestPostProcessors.csrf())
-                        .header("Authorization", "Bearer token"))
+                        .with(jwt().authorities(List.of(new SimpleGrantedAuthority("SCOPE_ADMIN")))
+                                .jwt(jwt -> jwt.claim(StandardClaimNames.PREFERRED_USERNAME, "admin")))
+                        .header("Authorization", ""))
                 .andExpect(status().isBadRequest());
     }
 }
