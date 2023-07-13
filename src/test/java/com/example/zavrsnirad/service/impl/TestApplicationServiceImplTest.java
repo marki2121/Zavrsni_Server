efc79bd359145cbc9ad44fd37c5c50752c636c1d
mmaruna <mmaruna@croz.hr>
2023-07-13 20:12:28 +0200

    added test file

---
+++ src/test/java/com/example/zavrsnirad/service/impl/TestApplicationServiceImplTest.java
@@ -0,0 +1,18 @@
+package com.example.zavrsnirad.service.impl;
+
+import org.junit.jupiter.api.Test;
+
+class TestApplicationServiceImplTest {
+
+    @Test
+    void applyForTest() {
+    }
+
+    @Test
+    void getAllApplications() {
+    }
+
+    @Test
+    void deleteApplication() {
+    }
+}
