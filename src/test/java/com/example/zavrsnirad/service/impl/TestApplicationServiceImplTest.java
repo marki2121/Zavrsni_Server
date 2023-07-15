7ec193a5a3d102c69164d264116ef4e670d7f5a8
mmaruna <mmaruna@croz.hr>
2023-07-15 11:30:35 +0200

    Tested applyForTest Fully

--- src/test/java/com/example/zavrsnirad/service/impl/TestApplicationServiceImplTest.java
+++ src/test/java/com/example/zavrsnirad/service/impl/TestApplicationServiceImplTest.java
@@ -19,6 +19,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Set;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
 
@@ -59,6 +60,101 @@ class TestApplicationServiceImplTest {
         //then
         ResponseEntity<Object> response = testApplicationService.applyForTest("token",1L);
         assertTrue(response.getStatusCode().is2xxSuccessful());
+    }
+
+    @Test
+    @DisplayName("Test applyForTest() and fail because user already applied")
+    void applyForTestFail() {
+        //given
+        Subject subject = new Subject(1L, "Subject", "asd",5, 1, 3, null);
+        Set<Subject> subjects = Set.of(subject);
+        User user = new User(1L, "username", "password", Role.STUDENT, true, null, subjects);
+        subject.setStudents(Set.of(user));
+        com.example.zavrsnirad.entity.Test test = new com.example.zavrsnirad.entity.Test(1L, subject, null, "da");
+
+        //when
+        when(tokenService.getUsernameFromToken("token")).thenReturn("username");
+        when(userRepository.findByUsername("username")).thenReturn(java.util.Optional.of(user));
+        when(testRepository.findById(1L)).thenReturn(java.util.Optional.of(test));
+        when(testApplicationRepository.findByStudentAndTest(user, test)).thenReturn(java.util.Optional.of(new com.example.zavrsnirad.entity.TestApplication()));
+
+        //then
+        ResponseEntity<Object> response = testApplicationService.applyForTest("token",1L);
+        assertTrue(response.getStatusCode().is4xxClientError());
+        assertEquals(response.getBody(), "You have already applied for this test");
+    }
+
+    @Test
+    @DisplayName("Test applyForTest() and fail because username is not found")
+    void applyForTestFail2() {
+        //when
+        when(tokenService.getUsernameFromToken("token")).thenReturn("username");
+
+        //then
+        ResponseEntity<Object> response = testApplicationService.applyForTest("token",1L);
+        assertTrue(response.getStatusCode().is4xxClientError());
+        assertEquals(response.getBody(), "User not found");
+    }
+
+    @Test
+    @DisplayName("Test applyForTest() and fail because test is not found")
+    void applyForTestFail3() {
+        //given
+        Subject subject = new Subject(1L, "Subject", "asd",5, 1, 3, null);
+        Set<Subject> subjects = Set.of(subject);
+        User user = new User(1L, "username", "password", Role.STUDENT, true, null, subjects);
+        subject.setStudents(Set.of(user));
+
+        //when
+        when(tokenService.getUsernameFromToken("token")).thenReturn("username");
+        when(userRepository.findByUsername("username")).thenReturn(java.util.Optional.of(user));
+
+        //then
+        ResponseEntity<Object> response = testApplicationService.applyForTest("token",1L);
+        assertTrue(response.getStatusCode().is4xxClientError());
+        assertEquals(response.getBody(), "Test not found");
+    }
+
+    @Test
+    @DisplayName("Test applyForTest() and fail because user is not enrolled in subject")
+    void applyForTestFail4() {
+        //given
+        Subject subject = new Subject(1L, "Subject", "asd",5, 1, 3, null);
+        User user = new User(1L, "username", "password", Role.STUDENT, true, null, null);
+        com.example.zavrsnirad.entity.Test test = new com.example.zavrsnirad.entity.Test(1L, subject, null, "da");
+        subject.setStudents(Set.of());
+
+        //when
+        when(tokenService.getUsernameFromToken("token")).thenReturn("username");
+        when(userRepository.findByUsername("username")).thenReturn(java.util.Optional.of(user));
+        when(testRepository.findById(1L)).thenReturn(java.util.Optional.of(test));
+
+        //then
+        ResponseEntity<Object> response = testApplicationService.applyForTest("token",1L);
+        assertTrue(response.getStatusCode().is4xxClientError());
+        assertEquals(response.getBody(), "You are not enrolled in this subject");
+    }
+
+    @Test
+    @DisplayName("Test applyForTest() and fail because user is already applied for test")
+    void applyForTestFail5() {
+        //given
+        Subject subject = new Subject(1L, "Subject", "asd",5, 1, 3, null);
+        Set<Subject> subjects = Set.of(subject);
+        User user = new User(1L, "username", "password", Role.STUDENT, true, null, subjects);
+        subject.setStudents(Set.of(user));
+        com.example.zavrsnirad.entity.Test test = new com.example.zavrsnirad.entity.Test(1L, subject, null, "da");
+
+        //when
+        when(tokenService.getUsernameFromToken("token")).thenReturn("username");
+        when(userRepository.findByUsername("username")).thenReturn(java.util.Optional.of(user));
+        when(testRepository.findById(1L)).thenReturn(java.util.Optional.of(test));
+        when(testApplicationRepository.findByStudentAndTest(user, test)).thenReturn(java.util.Optional.of(new com.example.zavrsnirad.entity.TestApplication()));
+
+        //then
+        ResponseEntity<Object> response = testApplicationService.applyForTest("token",1L);
+        assertTrue(response.getStatusCode().is4xxClientError());
+        assertEquals(response.getBody(), "You have already applied for this test");
     }
 
     @Test
