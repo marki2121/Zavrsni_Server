6243213dfabd8677ff2081979b52543493458776
mmaruna <mmaruna@croz.hr>
2023-07-15 12:14:23 +0200

    Started testing test service

---
+++ src/test/java/com/example/zavrsnirad/service/impl/TestServiceImplTest.java
@@ -0,0 +1,30 @@
+package com.example.zavrsnirad.service.impl;
+
+import org.junit.jupiter.api.Test;
+
+class TestServiceImplTest {
+
+    @Test
+    void createTest() {
+    }
+
+    @Test
+    void getTestsForSubject() {
+    }
+
+    @Test
+    void deleteTest() {
+    }
+
+    @Test
+    void updateTest() {
+    }
+
+    @Test
+    void getAllTestsApplications() {
+    }
+
+    @Test
+    void gradeTest() {
+    }
+}
